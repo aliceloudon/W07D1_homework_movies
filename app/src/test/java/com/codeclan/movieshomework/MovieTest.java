58903160386eea513f80f163ebba099c75ccb6f0
Alice Loudon <alice_loudon@icloud.com>
2017-03-13 15:16:12 +0000

    Add Movies and MoviesTest classes

---
+++ app/src/test/java/com/codeclan/movieshomework/MovieTest.java
@@ -0,0 +1,24 @@
+package com.codeclan.movieshomework;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by user on 13/03/2017.
+ */
+
+public class MovieTest {
+
+    Movie movie;
+
+    @Before
+    public void before(){
+        movie = new movie("Milk", "drama", 8.0);
+    }
+
+    @Test
+    public void testMovieSetup(){
+        assertEquals("Milk", movie.getTitle());
+    }
+    
+}
